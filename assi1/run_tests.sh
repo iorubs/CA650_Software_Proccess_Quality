a82201e63cbb03fb1e96f3b918e2315bc433a087
Ruben Vasconcelos <ruben.vasconcelos2@mail.dcu.ie>
2018-02-15 12:50:24 +0000

    Code cov

--- assi1/run_tests.sh
+++ assi1/run_tests.sh
@@ -4,6 +4,10 @@ mkdir build
 
 javac -d /app/build -cp /app/junit/junit-4.12.jar /app/tests/*.java /app/src/*.java
 
-java -cp /app/build:/app/junit/junit-4.12.jar:/app/junit/hamcrest-core-1.3.jar TestRunner
+java -jar /app/junit/jcov.jar Instr build/Stack.class
 
-rm -rf build
+java -cp /app/build:/app/junit/junit-4.12.jar:/app/junit/hamcrest-core-1.3.jar:/app/junit/jcov_file_saver.jar TestRunner
+
+java -jar /app/junit/jcov.jar RepGen -o report result.xml
+
+rm -rf build result.xml template.xml
